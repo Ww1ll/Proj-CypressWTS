4c1bc3a56cbcd086e0d5be56d4e03e59781f5226
Ww1ll <williambraz65@gmail.com>
2026-04-28 00:06:56 -0300

    feat add formato api

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -1,19 +1,34 @@
 import { faker } from '@faker-js/faker'
 
-it("should be able to add a todo", ()=> {
-    cy.visit('/signup');
-    cy.get('[data-testid="first-name"]').type(faker.person.firstName());
-    cy.get('[data-testid="last-name"]').type(faker.person.lastName());
-    cy.get('[data-testid="email"]').type(faker.internet.email());
-    cy.get('[data-testid="password"]').type('Test123456');
-    cy.get('[data-testid="confirm-password"]').type('Test123456');
-    cy.get('[data-testid="submit"]').click();
-    cy.get('[data-testid="welcome"]').should('be.visible');
+it.only("should be able to add a todo", ()=> {
+    
+    cy.request({
+        method: "POST",
+        url: '/api/v1/users/register',
+        body: {
+            firstName: faker.person.firstName() ,
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            password:'Test12345',
+        },
+    }).then((response) => {
+        let token = response.body.access_token;
+        cy.request({
+            method: 'POST',
+            url: '/api/v1/tasks',
+            body: {
+                isCompleted: false,
+                item: 'Learn Cypress'
+            },
+            headers: {
+                Authorization: `Bearer${token}`,
+            },
+        });
+    });
 
-    cy.get('[data-testid="add"]').click();
-    cy.get('[data-testid="new-todo"]').type('Learn Cypress with typescript');
-    cy.get('[data-testid="submit-newTask"]').click();
-    cy.get('[data-testid="todo-item"]').should('contain.text', 'Learn Cypress with typescript');
+
+
+
 
 });
 
